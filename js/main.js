c0da33ad5335745a1bab3403cf86de84411de0a4
Maneesh <maneesh@Maneeshs-MacBook-Pro.local>
2015-06-23 20:47:39 +0200

    Added in an active handler for css niceness later on

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 window.onload = function () {
 	/*Lets set up some variables*/
-	var head = document.head || document.getElementsByTagName('head')[0];
-    var style = document.createElement('style');
-    var css = '';
 	var vContainer = document.getElementsByClassName('vanilla-container')[0];
 	var visibleWidth = 0;
 	var vWrapper = vContainer.getElementsByClassName('vanilla-wrapper')[0];
@@ -13,14 +10,12 @@ window.onload = function () {
 	var slideControlLeft = vControls.getElementsByClassName('left')[0];
 	var activeIndex = 1;
 	var currentSlideValue = 0;
-	var styleAdded = false;
-
 
 	function testBrowser(){
 		var cssSupportCheck = (function(){
-		var testElement = document.createElement('div');
-		var vendorPrefixs = ['Khtml, Ms, O, Webkit'];
-		var vendorLength = vendorPrefixs.length;
+			var testElement = document.createElement('div');
+			var vendorPrefixs = ['Khtml, Ms, O, Webkit'];
+			var vendorLength = vendorPrefixs.length;
 			return function(propTest){
 				if (propTest in testElement.style ) return true; //Catch 'em early
 				propTest = propTest.replace(/^[a-z]/,function(val) {
@@ -34,20 +29,17 @@ window.onload = function () {
 				return false;
 			};
 		})();
-	 
 		if (cssSupportCheck('transform')) {
 		   document.documentElement.className += ' transform'; // Adds class to html tag if true;
 		}
 	}
-
-
 
 	function calculateWrapperWidth(){
 		visibleWidth = vContainer.clientWidth;
 		totalWidth = visibleWidth * (vSlides.length);
 	}
 
-	function generateCSS(){			
+	function writeScaffoldingCSS(){			
 		vWrapper.style.cssText = 'width:'+totalWidth+'px;';
 		for (var slideLoop = 0;slideLoop < vSlides.length; slideLoop++){
 			vSlides[slideLoop].style.cssText = 'width:'+visibleWidth+'px;';	
@@ -55,18 +47,37 @@ window.onload = function () {
 		
 	}
 
-	// function writeCSS(){
-	// 	style.type = 'text/css';
-	// 	style.appendChild(document.createTextNode(css));
-	// 	head.appendChild(style);
-	// 	styleAdded = true;
-	// }
+	function handleClassDelegation(){
+		for (var slideCount = 0;slideCount < vSlides.length; slideCount++){
+			vSlides[slideCount].className = vSlides[slideCount].className.replace(/\b active\b/,'');
+		}
+		vSlides[activeIndex-1].className += ' active';
+	}
+
+	function checkIfActive(){
+		for (var i = 0;i < vSlides.length; i++){
+			if (vSlides[i].className.indexOf('active') >= 0){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	function setActive(){
+		if (checkIfActive() == false){
+			for (var slideCount = 0;slideCount < vSlides.length; slideCount++){
+				vSlides[slideCount].className = vSlides[slideCount].className.replace(/\b active\b/,'');
+			}
+			vSlides[0].className += ' active';
+		}
+	}
 
 	function slideRight(){
 		if ( (activeIndex * visibleWidth) < totalWidth){
 			var amountToSlide = (activeIndex * visibleWidth)*-1;
 			currentSlideValue = amountToSlide;
 			activeIndex ++;
+			handleClassDelegation();
 		} else {
 			var amountToSlide =  (((activeIndex-1) * visibleWidth))*-1;
 			currentSlideValue = amountToSlide;
@@ -81,6 +92,7 @@ window.onload = function () {
 		if (currentSlideValue < 0) {
 			currentSlideValue = amountToSlide;
 			activeIndex--;
+			handleClassDelegation();
 			if ( document.getElementsByClassName('transform').length > 0){ // checks if browser can transform
 				vWrapper.style.cssText += '-moz-transform: translateX('+amountToSlide+'px) translateY(0px); -webkit-transform: translateX('+amountToSlide+'px) translateY(0px); -o-transform: translateX('+amountToSlide+'px) translateY(0px); -ms-transform: translateX('+amountToSlide+'px) translateY(0px); transform: translateX('+amountToSlide+'px) translateY(0px);';
 			}
@@ -99,21 +111,15 @@ window.onload = function () {
 
 	window.addEventListener('resize',function(){
 		calculateWrapperWidth();
-		generateCSS();
-		// writeCSS();
+		writeScaffoldingCSS()
 	});
 
 
+	
+	setActive();
 	testBrowser();
 	calculateWrapperWidth();
-	generateCSS();
-	// writeCSS();
-
-	
-
-
-
-
+	writeScaffoldingCSS();
 
 }
 
