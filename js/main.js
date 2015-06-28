234419271c25184faa258c36f433030ddc311731
Maneesh <mpchiba@gmail.com>
2015-06-28 21:39:59 +0200

    Making pretty

--- js/main.js
+++ js/main.js
@@ -63,6 +63,17 @@ window.onload = function () {
 		return false;
 	}
 
+	function disableControls(){
+		slideControlLeft.className = slideControlLeft.className.replace(/\b disabled\b/,'');
+		slideControlRight.className = slideControlRight.className.replace(/\b disabled\b/,'');
+		if (activeIndex == 1){
+			slideControlLeft.className += ' disabled';
+		}
+		if (activeIndex == vSlides.length){
+			slideControlRight.className += ' disabled';
+		}
+	}
+
 	function setActiveSlide(){
 		if (checkIfActive() == false){
 			for (var slideCount = 0;slideCount < vSlides.length; slideCount++){
@@ -77,7 +88,10 @@ window.onload = function () {
 			var amountToSlide = (activeIndex * visibleWidth)*-1;
 			currentSlideValue = amountToSlide;
 			activeIndex ++;
-			handleClassDelegation();
+			disableControls();
+			setTimeout(function(){
+				handleClassDelegation();
+			},300)
 		} else {
 			var amountToSlide =  (((activeIndex-1) * visibleWidth))*-1;
 			currentSlideValue = amountToSlide;
@@ -94,7 +108,10 @@ window.onload = function () {
 		if (currentSlideValue < 0) {
 			currentSlideValue = amountToSlide;
 			activeIndex--;
-			handleClassDelegation();
+			disableControls();
+			setTimeout(function(){
+				handleClassDelegation();
+			},300)
 			if ( document.getElementsByClassName('transform').length > 0){ // checks if browser can transform
 				vWrapper.style.cssText += '-moz-transform: translateX('+amountToSlide+'px) translateY(0px); -webkit-transform: translateX('+amountToSlide+'px) translateY(0px); -o-transform: translateX('+amountToSlide+'px) translateY(0px); -ms-transform: translateX('+amountToSlide+'px) translateY(0px); transform: translateX('+amountToSlide+'px) translateY(0px);';
 			} else {
@@ -122,6 +139,84 @@ window.onload = function () {
 	testBrowser();
 	calculateWrapperWidth();
 	writeScaffoldingCSS();
+	disableControls();
+
+
+
+	//The following code ustilizes the YouTube API because I have not place to locally host and I wanted to play with the API for fun :)
+
+	var videos = vContainer.getElementsByClassName('video-player');
+	var videoIDs = [];
+	var players;
+	for (var videoPlayerCount = 0; videoPlayerCount < videos.length; videoPlayerCount++){
+		videos[videoPlayerCount].setAttribute('id','video'+videoPlayerCount);
+		videoIDs[videoPlayerCount] = videos[videoPlayerCount].getAttribute('data-yt-id');
+	}
+	// console.log(videoIDs);
+
+	// function onYouTubeIframeAPIReady(){
+	// 	for(var index = 0; index < videoIDs.length; index++){
+	// 		players[index] = new YT.Player('video'+index,{
+	// 			playerVars: { 'autoplay': 1, 'controls': 0 },
+	// 			height: '100%',
+	// 			width: '100%',
+	// 			videoId: videoIDs[index],
+	// 			events: {
+	// 				'onReady': onPlayerReady,
+	// 				'onStateChange': onPlayerStateChange
+	// 			}
+	// 		});
+	// 		console.log('video'+index);
+	// 	}
+	// }
+
+      // 2. This code loads the IFrame Player API code asynchronously.
+      // var tag = document.createElement('script');
+
+      // tag.src = "https://www.youtube.com/iframe_api";
+      // var firstScriptTag = document.getElementsByTagName('script')[0];
+      // firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      // 3. This function creates an <iframe> (and YouTube player)
+      //    after the API code downloads.
+
+
+      // var player;
+      // function onYouTubeIframeAPIReady() {
+      //   player = new YT.Player('player', {
+      //   playerVars: { 'autoplay': 1, 'controls': 0 },
+      //     height: '100%',
+      //     width: '100%',
+      //     videoId: '1FgLlwfY51Q',
+      //     events: {
+      //       'onReady': onPlayerReady,
+      //       'onStateChange': onPlayerStateChange
+      //     }
+      //   });
+      // }
+
+      // 4. The API will call this function when the video player is ready.
+
+
+      // function onPlayerReady(event) {
+      //   event.target.playVideo();
+      // }
+
+      // 5. The API calls this function when the player's state changes.
+      //    The function indicates that when playing a video (state=1),
+      //    the player should play for six seconds and then stop.
+
+
+      // var done = false;
+      // function onPlayerStateChange(event) {
+      //   if (event.data == YT.PlayerState.PLAYING && !done) {
+      //     setTimeout(stopVideo, 6000);
+      //     done = true;
+      //   }
+      // }
+      // function stopVideo() {
+      //   player.stopVideo();
+      // }
 
 }
 
